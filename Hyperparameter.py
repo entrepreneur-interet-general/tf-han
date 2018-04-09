5c351e9a4d93495c79bc8842b4fa09d5fa9213ae
vict0rsch <vsch@protonmail.com>
2018-04-09 18:47:09 +0200

    add str and repr

--- Hyperparameter.py
+++ Hyperparameter.py
@@ -8,15 +8,25 @@ class HP(object):
 
     @staticmethod
     def load(path_to_HP):
-        """[summary]
-        
+        """Static method: returns a HP object, located at path_to_HP.
+        If path_to_HP is a directory, will return the first .pkl file
+
         Args:
             path_to_HP ([type]): [description]
-        
+
         Returns:
             [type]: [description]
         """
         path = pathlib.Path(path_to_HP)
+        if path.is_dir():
+            files = [
+                f for f in path.iterdir()
+                if '.pkl' in str(f)
+            ]
+            print(files)
+            if not files:
+                raise FileNotFoundError('No .pkl file in provided path')
+            path = files[0]
         with path.open('rb') as f:
             hp = pkl.load(f)
         return hp
@@ -25,14 +35,36 @@ class HP(object):
             self,
             version='v1',
             base_dir_name=None):
+        self.__str_no_k = set(['dir'])
+        self.__str_no_v = set(['method', 'function'])
         self._path = ''
         self.version = version
         self.path_initialized = False
         self.now = datetime.datetime.now()
+        self.id = None
 
-        self.base_dir_name = base_dir_name or "%d-%d-%d" % (
-            self.now.year, self.now.month, self.now.day
+        self.base_dir_name = base_dir_name or str(self.now)[:10]
+
+    def __str__(self):
+        """Returns a string representation of the Hyperparameter:
+        no methods, no functions are included. The 'dir' attribute
+        is also ignored as it would create a directory if not already
+        existing
+
+        Returns:
+            str: string representation of the hyperparameter
+        """
+        return '\n'.join(
+            '{:25s} : {:10s}'.format(k, str(self.__getattribute__(k)))
+            for k in sorted(dir(self))
+            if '__' not in k and
+            k not in self.__str_no_k and
+            not any(v in str(self.__getattribute__(k))
+                    for v in self.__str_no_v)
         )
+
+    def __repr__(self):
+        return '<%s %s>' % (self.__class__, self.id)
 
     def save(self, name=''):
         """Dumps the Hyperparameter as a pickle file in hp.dir as:
@@ -80,18 +112,24 @@ class HP(object):
             paths = [
                 p.resolve() for p in path.iterdir()
                 if p.is_dir() and
-                self.base_dir_name in str(p)
+                self.base_dir_name in str(p) and
+                '_' in str(p)
             ]
             if paths:
                 _id = max(
                     [0] + [
                         int(str(p).split("_")[-1])
-                        for p in paths if '_' in str(p)]
+                        for p in paths]
                 ) + 1
-                path /= self.base_dir_name + '_' + str(_id)
+                new_name = self.base_dir_name + '_%d' % _id
+                self.id = self.version + ' | ' + new_name
             else:
-                path /= self.base_dir_name
+                new_name = self.base_dir_name
+                self.id = self.version + ' | ' + new_name
+
+            path /= new_name
             path.mkdir()
+
             self._path = path
             self.path_initialized = True
 
