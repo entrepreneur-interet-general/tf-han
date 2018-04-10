4ad98617bfc46402ea4d9478478d5008d2363609
vict0rsch <vsch@protonmail.com>
2018-04-10 16:23:42 +0200

    use logits and set embedding matrix

--- source/Model.py
+++ source/Model.py
@@ -5,7 +5,7 @@ class Model(object):
     def __init__(self, hp):
         self.hp = hp
         self.loss = None
-        self.output_layer = None
+        self.logits = None
 
         self.output_labels = tf.placeholder(
             tf.float32,
@@ -19,22 +19,26 @@ class Model(object):
             tf.float32,
             shape=(
                 self.hp.batch_size,
-                self.hp.max_sent_len,
                 self.hp.max_doc_len
+                self.hp.max_sent_len,
             )
         )
 
-    def set_output_layer(self):
-        raise NotImplementedError('set_output_layer should be implemented')
+    def set_logits(self):
+        raise NotImplementedError('set_logits should be implemented')
+
+    def set_embedding_matrix(self):
+        raise NotImplementedError('set_embedding_matrix should be implemented')
 
     def set_loss(self):
         self.loss = tf.reduce_mean(
             tf.nn.sigmoid_cross_entropy_with_logits(
-                logits=self.output_layer,
+                logits=self.logits,
                 labels=self.output_labels,
                 name='sig-xent'),
             name="mean-sig-xent")
 
     def build(self):
-        self.set_output_layer()
+        self.set_embedding_matrix()
+        self.set_logits()
         self.set_loss()
