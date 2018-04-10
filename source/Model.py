b9529bee7bb7d7d6de1f8451affceb0129147f24
vict0rsch <vsch@protonmail.com>
2018-04-10 20:22:53 +0200

    add graph and update input layer

--- source/Model.py
+++ source/Model.py
@@ -2,27 +2,30 @@ import tensorflow as tf
 
 
 class Model(object):
-    def __init__(self, hp):
+    def __init__(self, hp, graph=None):
         self.hp = hp
         self.loss = None
         self.logits = None
 
-        self.output_labels = tf.placeholder(
-            tf.float32,
-            shape=(
-                self.hp.batch_size,
-                self.hp.num_classes
-            ),
-            name="output_labels"
-        )
-        self.input_layer = tf.placeholder(
-            tf.float32,
-            shape=(
-                self.hp.batch_size,
-                self.hp.max_doc_len,
-                self.hp.max_sent_len,
+        self.graph = graph or tf.Graph()
+        with self.graph.as_default():
+            self.output_labels = tf.placeholder(
+                tf.float32,
+                shape=(
+                    self.hp.batch_size,
+                    self.hp.num_classes
+                ),
+                name="output_labels"
             )
-        )
+            self.input_layer = tf.placeholder(
+                tf.int32,
+                shape=(
+                    self.hp.batch_size,
+                    self.hp.max_doc_len,
+                    self.hp.max_sent_len,
+                ),
+                name='input_layer'
+            )
 
     def set_logits(self):
         raise NotImplementedError('set_logits should be implemented')
@@ -39,6 +42,7 @@ class Model(object):
             name="mean-sig-xent")
 
     def build(self):
-        self.set_embedding_matrix()
-        self.set_logits()
-        self.set_loss()
+        with self.graph.as_default():
+            self.set_embedding_matrix()
+            self.set_logits()
+            self.set_loss()
