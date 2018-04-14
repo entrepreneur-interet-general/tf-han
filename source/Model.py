50545718b40b9ab3adefa5641797ba4c0bfc331b
vict0rsch <vsch@protonmail.com>
2018-04-14 16:31:44 +0200

    doc

--- source/Model.py
+++ source/Model.py
@@ -2,7 +2,29 @@ import tensorflow as tf
 
 
 class Model(object):
+    """Abstract class for models to feed into a Trainer
+
+    Raises:
+        NotImplementedError: set_logits
+        NotImplementedError: set_embedding_matrix
+    """
+
     def __init__(self, hp, graph=None):
+        """Create a model with attributes:
+            * hyperparameter
+            * loss
+            * prediction
+            * input and labels tensors
+
+        Loss is always a sigmoid_cross_entropy_with_logits.
+        The child class defines the model's structure through
+        set_embedding_matrix and set_logits
+
+        Args:
+            hp (Hyperparameter): the model's params
+            graph (tf.Graph, optional): Defaults to None.
+                If none is provided, a new one is created
+        """
         self.hp = hp
         self.loss = None
         self.logits = None
@@ -13,12 +35,29 @@ class Model(object):
         self.graph = graph or tf.Graph()
 
     def set_logits(self):
+        """Should define the model's structure
+        through its output layer: model.logits which
+        will be used to compute loss and predictions
+
+        Raises:
+            NotImplementedError: Abstract method
+        """
         raise NotImplementedError('set_logits should be implemented')
 
     def set_embedding_matrix(self):
-        raise NotImplementedError('set_embedding_matrix should be implemented')
+        """Should define how the model will lookup the indexes
+        it will get as inputs.
+
+        Raises:
+            NotImplementedError: Abstract method
+        """
+        raise NotImplementedError(
+            'set_embedding_matrix should be implemented')
 
     def set_loss(self):
+        """Creates the loss operation as a component-wise logistic
+        regression by applying sigmoid_cross_entropy_with_logits
+        """
         with self.graph.as_default():
             with tf.name_scope('loss'):
                 self.loss = tf.reduce_mean(
@@ -29,6 +68,15 @@ class Model(object):
                     name="mean-sig-xent")
 
     def build(self, input_tensor, labels_tensor):
+        """Computes the major operations to create a model:
+        * set_embedding_matrix
+        * set_logits
+        * set_loss
+
+        Args:
+            input_tensor (tf.Variable): batch x docs x sentences
+            labels_tensor (tf.Variable): batch x num_classes
+        """
         with self.graph.as_default():
             with tf.name_scope('model'):
                 self.input_tensor = input_tensor
