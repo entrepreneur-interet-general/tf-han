9fcbf93d206c0f56e9351faa6fdf435576721c51
vict0rsch <vsch@protonmail.com>
2018-05-15 10:05:36 +0200

    add f1 and is_prop

--- source/utils.py
+++ source/utils.py
@@ -96,3 +96,50 @@ def task_specific_attention(inputs, output_size,
         outputs = tf.reduce_sum(weighted_projection, axis=1)
 
         return outputs
+
+
+def f1_score(y_true, y_pred):
+    """Computes 3 different f1 scores, micro macro
+    weighted.
+    micro: f1 score accross the classes, as 1
+    macro: mean of f1 scores per class
+    weighted: weighted average of f1 scores per class,
+              weighted from the support of each class
+
+
+    Args:
+        y_true (Tensor): labels, with shape (batch, num_classes)
+        y_pred (Tensor): model's predictions, same shape as y_true
+
+    Returns:
+        tupe(Tensor): (micro, macro, weighted)
+                      tuple of the computed f1 scores
+    """
+
+    f1s = [0, 0, 0]
+
+    y_true = tf.cast(y_true, tf.float64)
+    y_pred = tf.cast(y_pred, tf.float64)
+
+    for i, axis in enumerate([None, 0]):
+        TP = tf.count_nonzero(y_pred * y_true, axis=axis)
+        FP = tf.count_nonzero(y_pred * (y_true - 1), axis=axis)
+        FN = tf.count_nonzero((y_pred - 1) * y_true, axis=axis)
+
+        precision = TP / (TP + FP)
+        recall = TP / (TP + FN)
+        f1 = 2 * precision * recall / (precision + recall)
+
+        f1s[i] = tf.reduce_mean(f1)
+
+    weights = tf.reduce_sum(y_true, axis=0)
+    weights /= tf.reduce_sum(weights)
+
+    f1s[2] = tf.reduce_sum(f1 * weights)
+
+    micro, macro, weighted = f1s
+    return micro, macro, weighted
+
+
+def is_prop(obj, attr):
+    return isinstance(getattr(type(obj), attr, None), property)
