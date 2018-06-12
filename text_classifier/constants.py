9a6c450ee4e43a95166b358dcb45e766e1a29452
vict0rsch <vsch@protonmail.com>
2018-06-12 16:08:15 +0200

    package-wise constants

---
+++ text_classifier/constants.py
@@ -0,0 +1,21 @@
+split_doc_token = "|&|"
+padding_token = "<PAD>"
+randomizable_params = {
+    "cell_size": [1, 5, 1, "range", "uniform"],
+    "rnn_layers": [1, 2, 1, "range", "uniform"],
+    "batch_size": [32, 64, 32, "range", "uniform"],
+    "dropout": [0.5, 0.9, 0.05, "range", "uniform"],
+    "embedding_dim": [1, 10, 5, "range", "uniform"],
+    "epochs": [1, 2, 1, "range", "uniform"],
+    "learning_rate": [
+        0.00001,
+        0.00005,
+        0.0001,
+        0.0005,
+        0.001,
+        0.005,
+        0.01,
+        "list",
+        "uniform",
+    ],
+}
