54bacbc675cbc7ace9f08be6b93b12804943450f
vict0rsch <vsch@protonmail.com>
2018-06-13 14:30:11 +0200

    use default Munch

--- text_classifier/Experiment.py
+++ text_classifier/Experiment.py
@@ -6,49 +6,28 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+import yaml
+from munch import DefaultMunch
 
 from .constants import metrics, randomizable_params
 from .hyperparameters import HP
 from .trainers import DST
-from .utils import EndOfExperiment, get_new_dir, normal_choice, uniform_choice
-
-
-class Tee:
-    def write(self, *args, **kwargs):
-        self.file.write(*args, **kwargs)
-        self.stdout.write(*args, **kwargs)
-        self.flush()
-
-    def flush(self):
-        self.file.flush()
-        self.stdout.flush()
-
-    def __init__(self, path):
-        self.file = open(path, "a")
-        self.stdout = sys.stdout
-
-    def __del__(self):
-        self.file.close()
-        sys.stdout = self.stdout
-
-    def reset_stdout(self):
-        sys.stdout = self.stdout
+from .utils.utils import (
+    EndOfExperiment,
+    get_new_dir,
+    normal_choice,
+    uniform_choice,
+    Tee,
+)
 
 
 class Experiment(object):
-    @staticmethod
-    def from_json(path):
-        hp = HP.load(path, file_type="json")
-        return Experiment(hp.experiments_dir, hp)
-
     def __init__(self, experiments_dir="./experiments", conf_path=None, hp=None):
 
         self.conf_path = conf_path
         self.conf = None
-        self.experiments_dir = Path(experiments_dir)
         self.log = None
         self.trainer = None
-        self.params = None
         self.tee = None
         self.summary = {
             "params": {p: [] for p in randomizable_params},
@@ -58,48 +37,69 @@ class Experiment(object):
         self.current_run = 0
 
         if self.conf_path:
-            with open(Path(conf_path).resolve(), "r") as f:
-                self.conf = json.load(f)
-
-        if self.conf:
-            self.trainer_type = self.conf["trainer_type"]
-            if self.conf["exp_id"]:
-                self.exp_id = self.conf["exp_id"]
-            else:
-                self.exp_id = str(datetime.datetime.now())[:10]
+            self.set_conf(self.conf_path)
+            if experiments_dir:
+                self.conf.experiments_dir = experiments_dir
+            if not self.conf.exp_id:
+                self.conf.exp_id = str(datetime.datetime.now())[:10]
         else:
-            self.trainer_type = "DST"
-            self.exp_id = str(datetime.datetime.now())[:10]
+            self.conf = DefaultMunch(None)
+            self.conf.exp_id = str(datetime.datetime.now())[:10]
+            self.conf.trainer_type = "DST"
+            self.conf.randomizable_params = DefaultMunch(None).fromDict(
+                randomizable_params
+            )
+            self.conf.experiments_dir = experiments_dir
 
-        if not self.experiments_dir.exists():
-            print("Creating %s" % str(self.experiments_dir))
-            self.experiments_dir.mkdir(parents=True)
+        assert self.conf.experiments_dir is not None
 
-        self.dir = get_new_dir(self.experiments_dir, self.exp_id)
+        self.conf.experiments_dir = Path(self.conf.experiments_dir).resolve()
+
+        if not self.conf.experiments_dir.exists():
+            print("Creating %s" % str(self.conf.experiments_dir))
+            self.conf.experiments_dir.mkdir(parents=True)
+
+        self.dir = get_new_dir(self.conf.experiments_dir, self.conf.exp_id)
+
+    def set_conf(self, path):
+        with open(path, "r") as f:
+            conf = yaml.safe_load(f)
+        self.conf = DefaultMunch(None).fromDict(conf)
 
     def randomize(self, conf=None, verbose=0):
         conf = conf or self.conf
-        if conf:
-            self.params = conf["randomizable_params"]
-        else:
-            self.params = randomizable_params
+        params = conf.randomizable_params
 
-        for param in self.params:
-            distribution = self.params[param]
-
-            if distribution[-2] == "range":
-                values = np.arange(*distribution[:3])
+        for p_name, p in params.items():
+            if p.type == "range":
+                values = np.arange(p.min, p.max, p.step)
+            elif p.type == "list":
+                values = np.array(p.vals)
+            elif p.type == "fixed":
+                value = np.array(p.value)
             else:
-                values = np.array(distribution[:-2])
+                raise ValueError("Unkonw type {} for {}".format(p.type, p_name))
 
-            if distribution[-1] == "normal":
+            if p.distribution == "normal":
                 value = normal_choice(values)
-            else:
+            elif p.distribution == "uniform":
                 value = uniform_choice(values)
-            setattr(self.trainer.hp, param, value.tolist())
-            self.summary["params"][param].append(value)
+
+            setattr(self.trainer.hp, p_name, value.tolist())
+            self.summary["params"][p_name].append(value)
             if verbose > 0:
-                print("{:20}: {:10}".format(param, value))
+                print("{:20}: {:10}".format(p_name, value))
+
+    def dump_conf(self, path):
+        stringified = []
+        for attr, val in self.conf.items():
+            if isinstance(val, Path):
+                self.conf[attr] = str(val)
+                stringified.append(attr)
+        with open(path, "w") as f:
+            yaml.safe_dump(self.conf, f, default_flow_style=False)
+        for attr in stringified:
+            self.conf[attr] = Path(self.conf[attr])
 
     def summarize(self):
         metrics = pd.DataFrame(self.summary["metrics"])
@@ -114,10 +114,14 @@ class Experiment(object):
             f.write(summary)
         metrics.to_csv(self.dir / "metrics.csv")
         params.to_csv(self.dir / "params.csv")
+        self.dump_conf(self.dir / "conf.yaml")
 
     def setup(self, log=True):
-        if self.trainer_type == "DST":
+        if self.conf.trainer_type == "DST":
             hp = HP(base_dir=self.dir)
+            for attr, val in self.conf.data_paths.items():
+                if val is not None:
+                    setattr(hp, attr, val)
             self.trainer = DST(hp=hp)
         else:
             raise ValueError("Unknown Trainer")
@@ -141,7 +145,10 @@ class Experiment(object):
         self.summary["metrics"]["weighted_f1"].append(wei)
         self.summary["metrics"]["accuracy"].append(acc)
 
-    def run(self, n_runs=np.iinfo(int).max, randomize=True, log=True, verbose=0):
+    def run(self, n_runs=None, randomize=True, log=True, verbose=0):
+        n_runs = n_runs or self.conf.n_runs
+        if n_runs is None:
+            n_runs = np.iinfo(int).max
         print("\n= = > Run", self.current_run)
 
         self.setup(log)
