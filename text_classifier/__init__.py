4d9fb13f1cc5cf03d8535ed744b94a16abb44797
vict0rsch <vsch@protonmail.com>
2018-07-09 15:21:16 +0200

    add path

--- text_classifier/__init__.py
+++ text_classifier/__init__.py
@@ -1,5 +1,8 @@
 import builtins
+import sys
+import os
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 from .experiment import Experiment
 from .hyperparameters import HP
